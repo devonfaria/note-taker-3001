6b4d3a539fbda2e621fb8c135ad5ee031e555eef
Devon Faria <devonfaria@gmail.com>
2022-03-25 17:55:49 -0400

    created post method to capture the input from notes.html

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
-# note-taker-3001
+# Note Taker 3001
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+## Table of Contents
+
+* [Description](#description)
+* [Visuals](#visuals)
+* [Usage](#usage)
+* [Technologies](#technologies)
+* [Licenses](#licenses)
+* [Support](#support)
+* [Authors](#authors)
+* [Contributions](#contributions)
+
+## Description
+
+Welcome to the Note Taker 3001, where you can save your notes on a webpage so that you can access that information anywhere you have internet. You can save and delete notes, and then display them larger when you click on them. SImple, and easy to use!
+
+## Visuals
+
+The following image is of the landing page for the Note Taker 3001.
+
+![note taker 3001 main page](./images/Note-Taker-start.png)
+
+## Usage
+
+To enjoy this application, you will visit the URL at this [link.](https://floating-hollows-16544.herokuapp.com/)
+
+## Technologies
+
+* Node JS
+* Express
+
+## Licenses
+
+This application uses the MIT Software License. [Click here to view full license.](LICENSE)
+
+## Support
+
+Contact us at devonfaria@gmail.com if you need assistance downloading or activating this repository. You can also find me on [GitHub.](https://github.com/devonfaria)
+
+## Authors
+
+Devon Faria
+
+## Contributions
+
--- server.js
+++ server.js
@@ -1,5 +1,3 @@
-import uniqid from 'uniqid';
-
 const express = require('express');
 // Built in Node JS Module that corrects path names for you, like repeated forward slashes, forges paths together safely
 const path = require('path');
@@ -16,15 +14,106 @@ app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-// app.get('/', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'public/index.html'))
+
+// Enables get function to see notes page
+app.get('/api/notes', (req, res) => {
+  fs.readFile('.db/db.json', 'utf-8', function (err, data) {
+    console.log('Data from Notes', data);
+  })
+});
+
+// Accepts data from front end and converts to JSON for storage
+app.post('/api/notes', (req, res) => {
+  // Constructor for note object from request
+  const { title, text } = req.body;
+
+  if (title && text) {
+    // Variable for the object we will save
+    const newNote = {
+      title,
+      text,
+      note_id: uniqid(),
+    };
+
+    // Convert the data to a string
+    const noteString = JSON.stringify(newNote, null, 2);
+
+// Some type of fs function
+
+    const response = {
+      status: 'success',
+      body: newNote,
+    };
+// Come back and delete thissss
+    console.log(response);
+    res.status(201).json(response);
+  } else {
+    res.status(500).json('Error in posting review');
+  }
+});
+
+
+//   fs.writeFile('.db/db.json', res, function (err, data) {
+//     console.log('Data from Notes', data);
+//   })
 // });
 
+// Loads index.html page
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname,'/public/notes.html'))
+});
+
+// Loads index.html page
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '/public/index.html'))
-})
+});
 
 // Localhost 3001
 app.listen(PORT, () =>
   console.log(`Example app listening at http://localhost:${PORT}`)
 );
+
+
+
+// POST request to add a review
+app.post('/api/reviews', (req, res) => {
+  // Log that a POST request was received
+  console.info(`${req.method} request received to add a review`);
+
+  // Destructuring assignment for the items in req.body
+  const { product, review, username } = req.body;
+
+  // If all the required properties are present
+  if (product && review && username) {
+    // Variable for the object we will save
+    const newReview = {
+      product,
+      review,
+      username,
+      upvotes: Math.floor(Math.random() * 100),
+      review_id: uuid(),
+    };
+
+    // Convert the data to a string so we can save it
+    const reviewString = JSON.stringify(newReview, null, 2);
+
+    // Write the string to a file
+    fs.writeFile(`./db/${newReview.product}.json`, reviewString, (err) =>
+      err
+        ? console.error(err)
+        : console.log(
+            `Review for ${newReview.product} has been written to JSON file`
+          )
+    );
+
+    const response = {
+      status: 'success',
+      body: newReview,
+    };
+
+    console.log(response);
+    res.status(201).json(response);
+  } else {
+    res.status(500).json('Error in posting review');
+  }
+});
