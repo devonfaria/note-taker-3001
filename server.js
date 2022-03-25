1400ed986b22022841436d06f5ba1aea020a771a
Devon Faria <devonfaria@gmail.com>
2022-03-25 13:09:06 -0400

    added uniqid npm module to server.js

--- server.js
+++ server.js
@@ -1,18 +1,28 @@
+import uniqid from 'uniqid';
+
 const express = require('express');
 // Built in Node JS Module that corrects path names for you, like repeated forward slashes, forges paths together safely
 const path = require('path');
+const fs = require('fs');
+const uniqid = require('uniqid');
 
 // Executing express on line below
 const app = express();
-// All caps means DO NOT CHANGE THIS VARISBLE
+// All caps means DO NOT CHANGE THIS VARIABLE
 const PORT = process.env.PORT || 3001;
 
 // Exposes the files in public folder to the open web
 app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public/index.html'))
-});
+// app.get('/', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'public/index.html'))
+// });
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+})
 
 // Localhost 3001
 app.listen(PORT, () =>
